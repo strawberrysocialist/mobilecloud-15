b1cfea8f156cba7809ee40ddb4f3e7c3d5c2615b
John Partridge <john.r.partridge@gmail.com>
2015-06-26 18:06:39 -0700

    Basic outline of controller solution.

--- assignments/assignment2/src/main/java/org/magnum/dataup/AnEmptyController.java
+++ assignments/assignment2/src/main/java/org/magnum/dataup/AnEmptyController.java
@@ -17,7 +17,18 @@
  */
 package org.magnum.dataup;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.magnum.dataup.model.Video;
+import org.magnum.dataup.model.VideoStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class AnEmptyController {
@@ -38,5 +49,64 @@ public class AnEmptyController {
                                                                                                                                                                                                                                                                         
 	 * 
 	 */
+
+	public static final String VIDEO_ID_PATH = VideoSvcApi.VIDEO_SVC_PATH + "/{id}";
 	
+	public static final String VIDEOS_PATH = VideoSvcApi.VIDEO_SVC_PATH + "s";
+	
+	private AtomicLong idGenerator = new AtomicLong(0L);
+	
+	private List<Video> videos = new ArrayList<Video>();
+	
+	/**
+	 * This method extracts the metadata for a new Video and stores it in memory.
+	 * It returns a unique ID to the client for use when uploading the actual video.
+	 * @param v
+	 * @return
+	 */
+	@RequestMapping(value=VideoSvcApi.VIDEO_SVC_PATH, method=RequestMethod.POST)
+	public @ResponseBody long addVideoMetadata(Video v) {
+		// TODO Implement the logic to store the metadata.
+		v.setId(idGenerator.incrementAndGet());
+		synchronized(videos) {
+			videos.add(v);
+		}
+		return v.getId();
+	}
+
+	/**
+	 * This method extracts the metadata for a new Video and stores it in memory.
+	 * It returns a unique ID to the client for use when uploading the actual video.
+	 * @param v
+	 * @return
+	 */
+	@RequestMapping(value=VideoSvcApi.VIDEO_DATA_PATH, method=RequestMethod.POST)
+	public @ResponseStatus VideoStatus addVideo(@PathVariable long id) {
+		// TODO Implement the logic to store the video data.
+		return null;
+	}
+
+	/**
+	 * This method extracts the metadata for a new Video and stores it in memory.
+	 * It returns a unique ID to the client for use when uploading the actual video.
+	 * @param v
+	 * @return
+	 */
+	@RequestMapping(value=VIDEO_ID_PATH, method=RequestMethod.GET)
+	public Video getVideo(@PathVariable long id) {
+		// TODO Implement the logic to return the video for the given ID.
+		return null;
+	}
+
+	/**
+	 * This method extracts the metadata for a new Video and stores it in memory.
+	 * It returns a unique ID to the client for use when uploading the actual video.
+	 * @param v
+	 * @return
+	 */
+	@RequestMapping(value=VIDEOS_PATH, method=RequestMethod.GET)
+	public @ResponseBody List<Video> getVideos() {
+		// TODO Implement the logic to return the list of all videos.
+		return videos;
+	}
 }
